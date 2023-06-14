d3f5878e4913dfb5d65edb8b6f0c0a0ee9d534a4
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-14 17:13:38 -0400

    feat: add MainMenuItemButton storie

---
+++ src/components/MainMenuItemButton/MainMenuItemButton.stories.ts
@@ -0,0 +1,49 @@
+import MainMenuItemButton, { type MainMenuItemButtonProps } from '@/components/MainMenuItemButton/MainMenuItemButton.vue'
+
+import type { StoryObjProped } from '@/types/storybook.types';
+import type { Meta } from '@storybook/vue3';
+
+const meta = {
+  title: 'Components/MainMenu/ItemButton',
+  component: MainMenuItemButton,
+  argTypes: {
+
+    firstIconName: {
+      name: 'Icon',
+      options: ['api', 'phoneList'],
+      control: { type: 'radio' },
+    },
+    isCurrentRoute: {
+      name: 'Active'
+    },
+
+    routePath: {
+      name: 'Route Path',
+    },
+
+  },
+  tags: ['autodocs'],
+
+} satisfies Meta<typeof MainMenuItemButton>;
+
+export default meta;
+
+type Story = StoryObjProped<typeof MainMenuItemButton, MainMenuItemButtonProps>;
+
+
+export const Primary: Story = {
+  render: (args) => ({
+    components: { MainMenuItemButton },
+    setup() {
+      return { args }
+    },
+    template: '<MainMenuItemButton v-bind="args" />'
+  }),
+  args: {
+    firstIconName: 'api',
+    isCurrentRoute: false,
+    routePath: '/'
+  }
+}
+
+
