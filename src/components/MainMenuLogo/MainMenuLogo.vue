5f551204f406d22b8b9787e9955c3de8af3fe960
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-14 17:50:42 -0400

    feat: add MainMenuLogo component

---
+++ src/components/MainMenuLogo/MainMenuLogo.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import SvgIconImporter from '@/components/SvgIconImporter.vue';
+type IconName = 'logo'
+
+export interface MainMenuLogoProps {
+  iconName: IconName
+  isCurrentRoute: boolean
+}
+
+const props = defineProps<MainMenuLogoProps>()
+</script>
+
+<template>
+  <SvgIconImporter :class="{ 'active': props.isCurrentRoute }" :name="props.iconName" />
+</template>
+
+<style scoped>
+.active {
+  background: #FD9802;
+}
+</style>
