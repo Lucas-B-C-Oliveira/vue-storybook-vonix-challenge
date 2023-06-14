35f872f4752b118efe8e5a213df2643ec4cdcfb4
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-14 15:33:35 -0400

    feat: add SvgIconImporter component

---
+++ src/components/SvgIconImporter.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
+
+export interface Props {
+  name: string
+}
+
+const props = defineProps<Props>()
+
+
+const IconComponent = defineAsyncComponent(
+  () => import(`../assets/icons/${props.name}.svg`)
+)
+</script>
+
+<template>
+  <component :is="IconComponent" />
+</template>
+
+<style scoped></style>
