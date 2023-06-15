1db360e50eb913ffb6531de563bbcda94f29861f
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-15 18:11:45 -0400

    feat: add NavMenu stories

---
+++ src/components/NavigationMenu/NavMenu.stories.ts
@@ -0,0 +1,32 @@
+import NavMenu, { type NavMenuProps } from '@/components/NavigationMenu/NavMenu.vue'
+import { Mock } from '@/components/NavigationMenu/mock'
+import { vueRouter } from 'storybook-vue3-router'
+
+import type { StoryObjProped } from '@/types/storybook.types';
+import type { Meta } from '@storybook/vue3';
+
+const meta = {
+  title: 'Components/NavigationMenu/NavMenu',
+  component: NavMenu,
+
+  tags: ['autodocs'],
+
+} satisfies Meta<typeof NavMenu>;
+
+export default meta;
+
+type Story = StoryObjProped<typeof NavMenu, NavMenuProps>;
+
+
+export const Primary: Story = {
+  decorators: [vueRouter()],
+  render: (args) => ({
+    components: { NavMenu },
+    setup() {
+      return { args }
+    },
+    template: '<NavMenu v-bind="args" />'
+  }),
+  args: Mock.primary
+}
+
