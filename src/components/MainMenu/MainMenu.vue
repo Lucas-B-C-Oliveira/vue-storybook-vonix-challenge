b5cbac14121dc008455b17f35d62ed985458a614
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-15 12:22:49 -0400

    feat: add MainMenu component

---
+++ src/components/MainMenu/MainMenu.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import MainMenuItemButton, { type MainMenuItemButtonProps } from './ItemButton/MainMenuItemButton.vue';
+import MainMenuItemDropdown, { type MainMenuItemDropdownProps } from './ItemDropdown/MainMenuItemDropdown.vue';
+import MainMenuLogo, { type MainMenuLogoProps } from './Logo/MainMenuLogo.vue';
+
+
+export interface MainMenuProps {
+  logo: MainMenuLogoProps
+  dashboards: MainMenuItemDropdownProps
+  calls: MainMenuItemDropdownProps
+  conversations: MainMenuItemDropdownProps
+  employees: MainMenuItemDropdownProps
+  settings: MainMenuItemDropdownProps
+  api: MainMenuItemButtonProps
+  phoneList: MainMenuItemButtonProps
+  user: MainMenuItemDropdownProps
+}
+
+const props = defineProps<MainMenuProps>()
+
+</script>
+
+<template>
+  <nav class="container">
+    <div class="itemsGroup">
+      <MainMenuLogo v-bind="logo" />
+      <MainMenuItemDropdown v-bind="dashboards" />
+      <MainMenuItemDropdown v-bind="calls" />
+      <MainMenuItemDropdown v-bind="conversations" />
+      <MainMenuItemDropdown v-bind="employees" />
+      <MainMenuItemDropdown v-bind="settings" />
+    </div>
+    <div class="itemsGroup">
+      <MainMenuItemButton v-bind="api" />
+      <MainMenuItemButton v-bind="phoneList" />
+      <MainMenuItemDropdown v-bind="user" />
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  background-color: #3D55AE;
+  width: 100%;
+  height: 61px;
+  flex-direction: row;
+  justify-content: space-between;
+  border-bottom: 1px solid #C5C5C5;
+}
+
+.itemsGroup {
+  display: flex;
+  flex-direction: row;
+}
+</style>
