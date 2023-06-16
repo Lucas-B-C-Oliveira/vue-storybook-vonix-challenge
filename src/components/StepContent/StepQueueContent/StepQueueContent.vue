91f54c572e6268bec6f246c34132dd490e18db9b
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-16 17:23:27 -0400

    feat: add StepQueueContent component

---
+++ src/components/StepContent/StepQueueContent/StepQueueContent.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import StepQueueContentChooseQueue, { type StepQueueContentChooseQueueProps } from '@/components/StepContent/StepQueueContent/ChooseQueue/StepQueueContentChooseQueue.vue'
+
+export interface StepQueueContentProps {
+  queue: StepQueueContentChooseQueueProps
+}
+
+const props = defineProps<StepQueueContentProps>()
+
+</script>
+
+<template>
+  <section class="stepQueueContentContainer">
+    <StepQueueContentChooseQueue v-bind="queue" @chooseQueueSelectChange="$emit('chooseQueueSelectChange', $event)" />
+  </section>
+</template>
+
+<style scoped>
+.stepQueueContentContainer {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  max-width: 503px;
+  max-height: 619px;
+}
+</style>
