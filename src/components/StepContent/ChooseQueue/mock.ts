1f547bfdaa2ca2bcba4f237f24a62052346420b4
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-16 12:12:50 -0400

    feat: add StepContentChooseQueue mock

---
+++ src/components/StepContent/ChooseQueue/mock.ts
@@ -0,0 +1,23 @@
+import type { StepContentChooseQueueProps } from '@/components/StepContent/ChooseQueue/StepContentChooseQueue.vue'
+
+const primary: StepContentChooseQueueProps = {
+  labelText: 'Escolha a fila *',
+  queues: [
+    {
+      text: 'Escolha uma opção',
+      value: '',
+    },
+    {
+      text: 'Fila 1',
+      value: 'queue1',
+    },
+    {
+      text: 'Fila 2',
+      value: 'queue2',
+    },
+  ]
+}
+
+export const Mock = {
+  primary
+}
