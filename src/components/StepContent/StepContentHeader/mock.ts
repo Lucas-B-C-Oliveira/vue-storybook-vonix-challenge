ad268acb1d4cdbdca91f54ee673f3bb00750c897
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-15 20:54:06 -0400

    feat: add StepContentHeader mock

---
+++ src/components/StepContent/StepContentHeader/mock.ts
@@ -0,0 +1,19 @@
+import type { StepContentHeaderProps } from '@/components/StepContent/StepContentHeader/StepContentHeader.vue'
+
+const queue: StepContentHeaderProps = {
+  title: 'Fila'
+}
+
+const file: StepContentHeaderProps = {
+  title: 'Arquivo'
+}
+
+const fileMapping: StepContentHeaderProps = {
+  title: 'Mapeamento'
+}
+
+export const Mock = {
+  queue,
+  file,
+  fileMapping
+}
