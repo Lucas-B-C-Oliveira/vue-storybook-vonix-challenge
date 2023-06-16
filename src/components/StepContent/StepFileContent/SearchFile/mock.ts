69de4efaece8d0d25c02ae5987cad119b8194e14
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-16 19:56:03 -0400

    feat: add SearchFile mock

---
+++ src/components/StepContent/StepFileContent/SearchFile/mock.ts
@@ -0,0 +1,10 @@
+import { type SearchFileProps } from '@/components/StepContent/StepFileContent/SearchFile/SearchFile.vue'
+
+const primary: SearchFileProps = {
+  iconName: 'magnifyingGlass',
+  placeholderText: 'Pesquisar por arquivo'
+}
+
+export const Mock = {
+  primary,
+}
