574a3d2a21ca6b5d649eb5c2dcb1e75bd0801b6c
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-15 20:52:47 -0400

    feat: add StepContentHeader component

---
+++ src/components/StepContent/StepContentHeader/StepContentHeader.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+type StepTitle = 'Fila' | 'Arquivo' | 'Mapeamento'
+
+export interface StepContentHeaderProps {
+  title: string | StepTitle
+}
+
+defineProps<StepContentHeaderProps>()
+</script>
+
+<template>
+  <div class="stepHeaderContainer">
+    <h2 class="stepHeaderTitle">{{ title }}</h2>
+  </div>
+</template>
+
+<style scoped>
+.stepHeaderTitle {
+  font-family: 'Work Sans';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 23px;
+  display: flex;
+  align-items: center;
+  color: #444444;
+}
+
+.stepHeaderContainer {
+  padding-bottom: 16px;
+  border-bottom: 1px solid #CED4DA;
+  width: 100%;
+}
+</style>
