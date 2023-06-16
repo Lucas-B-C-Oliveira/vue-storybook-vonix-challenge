2ea6dffbb899135c2e5d3fbda6a0a777f93b2846
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-16 18:46:55 -0400

    feat: add DashboardsView component

---
+++ src/views/Dashboards/DashboardsView.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import HeaderComponent, { type HeaderComponentProps } from '@/components/Header/HeaderComponent.vue'
+import LeftSidebar, { type LeftSidebarProps } from '@/components/Sidebar/LeftSidebar.vue'
+import StepContent, { type StepContentProps } from '@/components/StepContent/StepContent.vue'
+
+export interface DashboardsViewProps {
+  headerComponentProps: HeaderComponentProps
+  leftSidebarProps: LeftSidebarProps
+  stepContentProps: StepContentProps
+}
+
+defineProps<DashboardsViewProps>()
+
+</script>
+
+<template>
+  <div class="dahsboardsContainer">
+    <HeaderComponent v-bind="headerComponentProps" @previousInGroupButtonClicked="$emit('previousInGroupButtonClicked')"
+      @nextInGroupButtonClicked="$emit('nextInGroupButtonClicked')" @nextButtonClicked="$emit('nextButtonClicked')"
+      @previousButtonClicked="$emit('previousButtonClicked')" />
+    <div class="bodyContainer">
+      <LeftSidebar v-bind="leftSidebarProps" />
+      <StepContent v-bind="stepContentProps" @chooseQueueSelectChange="$emit('chooseQueueSelectChange', $event)" />
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+.bodyContainer {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+}
+
+.dahsboardsContainer {
+  width: 1440px;
+  height: 769px;
+  display: flex;
+  flex-direction: column;
+  background: #FFFFFF;
+}
+</style>
