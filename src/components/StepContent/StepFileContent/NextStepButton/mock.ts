d88e7c5241df81f5a6eac9397ff89e68644caf9c
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-17 17:15:05 -0400

    feat: add NextStepButton mock

---
+++ src/components/StepContent/StepFileContent/NextStepButton/mock.ts
@@ -0,0 +1,10 @@
+import type { NextStepButtonProps } from "@/components/StepContent/StepFileContent/NextStepButton/NextStepButton.vue"
+
+const primary: NextStepButtonProps = {
+  buttonText: 'Ir para próxima etapa',
+  iconName: 'rightArrow'
+}
+
+export const Mock = {
+  primary,
+}
