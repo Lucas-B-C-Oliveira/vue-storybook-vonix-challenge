9ed5fe56c16b03116f37d117487b37d0f021b509
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-17 17:15:19 -0400

    feat: add FileNotFoundFeedback component

---
+++ src/components/StepContent/StepFileContent/FileNotFoundFeedback/FileNotFoundFeedback.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import SvgIconImporter from '@/components/SvgIconImporter.vue'
+
+export interface FileNotFoundFeedbackProps {
+  iconName: string
+  title: string
+  subtitle: string
+}
+
+defineProps<FileNotFoundFeedbackProps>()
+
+</script>
+
+<template>
+  <section class="fileNotFoundContainer">
+    <SvgIconImporter :name="iconName" />
+    <p class="titleText">{{ title }}</p>
+    <p class="subtitleText">{{ subtitle }}</p>
+  </section>
+</template>
+
+<style scoped>
+.fileNotFoundContainer {
+  display: flex;
+  flex-direction: column;
+  padding: 54px;
+  gap: 20px;
+  height: 100%;
+  width: 100%;
+  align-items: center;
+}
+
+.titleText {
+  font-family: 'Work Sans';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 14px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: #444444;
+}
+
+.subtitleText {
+  font-family: 'Work Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 14px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: #444444;
+}
+</style>
