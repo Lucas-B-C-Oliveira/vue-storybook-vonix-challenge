620d7192659945410f27f019a641e74c845fdbc1
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-16 20:47:19 -0400

    feat: add FileContentButton component

---
+++ src/components/StepContent/StepFileContent/FileContentButton/FileContentButton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import SvgIconImporter from '@/components/SvgIconImporter.vue'
+
+export interface FileContentButtonProps {
+  iconName: 'rightCaret' | 'leftCaret'
+}
+
+defineProps<FileContentButtonProps>()
+
+</script>
+
+<template>
+  <button class="fileButtonContainer">
+    <SvgIconImporter :name="iconName" />
+  </button>
+</template>
+
+<style scoped>
+.fileButtonContainer {
+  width: 24px;
+  height: 24px;
+  border: 1px solid #C5C5C5;
+  border-radius: 3px;
+}
+</style>
