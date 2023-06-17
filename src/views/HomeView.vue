55232a0a941e1576287bfc6502e1280fa67db636
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-16 22:28:32 -0400

    Revert "feat: add FileTable component"
    This reverts commit 28fa4eaadfe693c1e032beedfba995738226c5a7.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import NavMenuButton from '@/components/NavigationMenu/Button/NavMenuButton.vue'
+import NavMenu from '@/components/NavigationMenu/NavMenu.vue'
+// import { Mock } from '@/components/NavigationMenu/mock'
+import StepContentChooseQueue from '@/components/StepContent/ChooseQueue/StepContentChooseQueue.vue'
+import { Mock } from '@/components/StepContent/StepQueueContent/ChooseQueue/mock'
+
+const a = (event: any) => {
+  console.log('event', event)
+}
 
 </script>
 
 <template>
   <main style="padding: 10px;">
-
+    <h1>Home</h1>
+    <!-- <NavMenu @nextInGroupButtonClicked="console.log('MamaMia')" v-bind="Mock.primary" /> -->
+    <NavMenuButton text="Proximo" variant="nextOnly" @click="console.log('aaa')" />
+    <StepContentChooseQueue v-bind="Mock.primary" @chooseQueueSelectChange="a" />
   </main>
 </template>
