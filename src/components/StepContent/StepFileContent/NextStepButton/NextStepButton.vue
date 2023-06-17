c9c1dc13bd4217133ec9a5d35d698db5de0aa650
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-17 17:14:51 -0400

    feat: add NextStepButton component

---
+++ src/components/StepContent/StepFileContent/NextStepButton/NextStepButton.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import SvgIconImporter from '@/components/SvgIconImporter.vue';
+
+
+export interface NextStepButtonProps {
+  buttonText: string
+  iconName: 'rightArrow'
+}
+
+defineProps<NextStepButtonProps>()
+
+</script>
+
+<template>
+  <button class="nextStepButtonContainer">
+    {{ buttonText }}
+    <SvgIconImporter :name="iconName" />
+  </button>
+</template>
+
+<style scoped>
+.nextStepButtonContainer {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 18px;
+  gap: 5px;
+
+  width: 1073px;
+  height: fit-content;
+
+  background: #4B68D2;
+  border: 2px solid #4B68D2;
+  border-radius: 5px;
+
+  font-family: 'Work Sans';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 12px;
+
+  color: #FFFFFF;
+}
+</style>
