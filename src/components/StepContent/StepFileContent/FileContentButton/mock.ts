d6d0946d6940d79b004ed343c2c3c6d40aa9582f
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-16 20:47:31 -0400

    feat: add FileContentButton mock

---
+++ src/components/StepContent/StepFileContent/FileContentButton/mock.ts
@@ -0,0 +1,14 @@
+import { type FileContentButtonProps } from '@/components/StepContent/StepFileContent/FileContentButton/FileContentButton.vue'
+
+const leftButton: FileContentButtonProps = {
+  iconName: 'leftCaret'
+}
+
+const rightButton: FileContentButtonProps = {
+  iconName: 'rightCaret'
+}
+
+export const Mock = {
+  leftButton,
+  rightButton
+}
