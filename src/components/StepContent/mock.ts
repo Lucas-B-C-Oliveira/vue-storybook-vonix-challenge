2881cf49de547096f38a8a1bab4419696a12e1e6
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-19 20:46:32 -0400

    feat: add new mock

--- src/components/StepContent/mock.ts
+++ src/components/StepContent/mock.ts
@@ -1,15 +1,24 @@
 import { type StepContentProps } from '@/components/StepContent/StepContent.vue'
 import { Mock as StepQueueContentMock } from '@/components/StepContent/StepQueueContent/mock'
 import { Mock as StepContentHeaderMock } from '@/components/StepContent/StepContentHeader/mock'
+import { Mock as StepContentFileMock } from '@/components/StepContent/StepFileContent/mock'
 
 
 const queue: StepContentProps = {
   currentStep: 'queue',
   stepContentTitle: StepContentHeaderMock.queue,
-  stepContentQueue: StepQueueContentMock.primary
+  stepContentQueue: StepQueueContentMock.primary,
+  stepContentFile: StepContentFileMock.selectAFile,
+}
 
+const file: StepContentProps = {
+  currentStep: 'file',
+  stepContentTitle: StepContentHeaderMock.file,
+  stepContentQueue: StepQueueContentMock.primary,
+  stepContentFile: StepContentFileMock.selectAFile,
 }
 
 export const Mock = {
   queue,
+  file,
 }
