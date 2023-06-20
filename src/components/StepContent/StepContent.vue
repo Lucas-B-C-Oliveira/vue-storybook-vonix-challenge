5311a83c1130b4a7705e404d4e46a8a572d73198
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-19 20:45:59 -0400

    chore: add computeds

--- src/components/StepContent/StepContent.vue
+++ src/components/StepContent/StepContent.vue
@@ -2,6 +2,8 @@
 import type { StepContentHeaderProps } from '@/components/StepContent/StepContentHeader/StepContentHeader.vue'
 import StepQueueContent, { type StepQueueContentProps } from '@/components/StepContent/StepQueueContent/StepQueueContent.vue'
 import StepContentHeader from '@/components/StepContent/StepContentHeader/StepContentHeader.vue';
+import { computed } from 'vue'
+import StepFileContent, { type StepFileContentProps } from '@/components/StepContent/StepFileContent/StepFileContent.vue'
 
 type CurrentStep = 'queue' | 'file' | 'fileMapping'
 
@@ -9,37 +11,49 @@ export interface StepContentProps {
   stepContentTitle: StepContentHeaderProps
   currentStep: CurrentStep
   stepContentQueue?: StepQueueContentProps
+  stepContentFile?: StepFileContentProps
 }
 
-defineProps<StepContentProps>()
+const props = defineProps<StepContentProps>()
+
+const isTheQueueStep = computed(() => (props.currentStep === 'queue') && (typeof props.stepContentQueue !== 'undefined'))
+const isTheFileStep = computed(() => (props.currentStep === 'file') && (typeof props.stepContentFile !== 'undefined'))
 
 </script>
 
 <template>
-  <main class="stepContainer">
+  <main :class="{ 'stepContainerQueue': isTheQueueStep, 'stepContainer': !isTheQueueStep }">
     <StepContentHeader v-bind="stepContentTitle" />
-    <template v-if="(currentStep === 'queue') && (typeof stepContentQueue !== 'undefined')">
-      <StepQueueContent @chooseQueueSelectChange="$emit('chooseQueueSelectChange', $event)" v-bind="stepContentQueue" />
-    </template>
+    <StepQueueContent v-if="isTheQueueStep" @chooseQueueSelectChange="$emit('chooseQueueSelectChange', $event)"
+      v-bind="stepContentQueue" />
+    <StepFileContent v-else-if="isTheFileStep" v-bind="stepContentFile" />
   </main>
 </template>
 
 <style scoped>
-.stepContainer {
-
+.stepContainerQueue {
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   padding: 70px 530px 50px 100px;
   gap: 20px;
-
   min-width: 1133px;
   min-height: 619px;
-
   width: 100%;
   height: 100%;
-
   background: #FAFAFA;
+}
 
+.stepContainer {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 30px;
+  gap: 20px;
+  min-height: 619px;
+  width: 100%;
+  max-width: 1133px;
+  height: 100%;
+  background: #FAFAFA;
 }
 </style>
