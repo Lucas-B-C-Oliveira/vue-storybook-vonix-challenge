d2aa43f34f5c41bd3310ad9e1542841fd9674868
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-19 20:42:44 -0400

    feat: add new mock in DashboardsView

--- src/views/Dashboards/mock.ts
+++ src/views/Dashboards/mock.ts
@@ -2,7 +2,8 @@
 import type { DashboardsViewProps } from '@/views/Dashboards/DashboardsView.vue'
 import { Mock as HeaderComponentMock } from '@/components/Header/mock'
 import { Mock as SidebarMock } from '@/components/Sidebar/mock'
-import { Mock as StepQueueContentMock } from '@/components/StepContent/mock'
+import { Mock as StepContentMock } from '@/components/StepContent/mock'
+import { Mock as StepContentFileMock } from '@/components/StepContent/StepFileContent/mock'
 
 const queue: DashboardsViewProps = {
   headerComponentProps: {
@@ -31,9 +32,132 @@ const queue: DashboardsViewProps = {
       state: 'current'
     }
   },
-  stepContentProps: StepQueueContentMock.queue,
+  stepContentProps: StepContentMock.queue,
 }
+
+const fileSelectAFile: DashboardsViewProps = {
+  headerComponentProps: {
+    mainMenuProps: {
+      ...HeaderComponentMock.primary.mainMenuProps,
+      logo: {
+        ...HeaderComponentMock.primary.mainMenuProps.logo,
+        isCurrentRoute: true
+      },
+      dashboards: {
+        ...HeaderComponentMock.primary.mainMenuProps.dashboards,
+        isCurrentRoute: true,
+      }
+    },
+    navMenuProps: {
+      ...HeaderComponentMock.primary.navMenuProps,
+      buttonControllerState: {
+        state: 'buttons',
+      },
+    },
+  },
+  leftSidebarProps: {
+    ...SidebarMock.primary,
+    queue: {
+      ...SidebarMock.primary.queue,
+      state: 'finished'
+    },
+    file: {
+      ...SidebarMock.primary.file,
+      state: 'current'
+
+    },
+
+  },
+  stepContentProps: StepContentMock.file,
+}
+
+const fileNextStep: DashboardsViewProps = {
+  headerComponentProps: {
+    mainMenuProps: {
+      ...HeaderComponentMock.primary.mainMenuProps,
+      logo: {
+        ...HeaderComponentMock.primary.mainMenuProps.logo,
+        isCurrentRoute: true
+      },
+      dashboards: {
+        ...HeaderComponentMock.primary.mainMenuProps.dashboards,
+        isCurrentRoute: true,
+      }
+    },
+    navMenuProps: {
+      ...HeaderComponentMock.primary.navMenuProps,
+      buttonControllerState: {
+        state: 'buttons',
+      },
+    },
+  },
+  leftSidebarProps: {
+    ...SidebarMock.primary,
+    queue: {
+      ...SidebarMock.primary.queue,
+      state: 'finished'
+    },
+    file: {
+      ...SidebarMock.primary.file,
+      state: 'current'
+
+    },
+
+  },
+  stepContentProps: {
+    ...StepContentMock.file,
+    stepContentFile: {
+      ...StepContentFileMock.nextStep
+
+    }
+  },
+}
+
+const fileNotFound: DashboardsViewProps = {
+  headerComponentProps: {
+    mainMenuProps: {
+      ...HeaderComponentMock.primary.mainMenuProps,
+      logo: {
+        ...HeaderComponentMock.primary.mainMenuProps.logo,
+        isCurrentRoute: true
+      },
+      dashboards: {
+        ...HeaderComponentMock.primary.mainMenuProps.dashboards,
+        isCurrentRoute: true,
+      }
+    },
+    navMenuProps: {
+      ...HeaderComponentMock.primary.navMenuProps,
+      buttonControllerState: {
+        state: 'buttons',
+      },
+    },
+  },
+  leftSidebarProps: {
+    ...SidebarMock.primary,
+    queue: {
+      ...SidebarMock.primary.queue,
+      state: 'finished'
+    },
+    file: {
+      ...SidebarMock.primary.file,
+      state: 'current'
+
+    },
+
+  },
+  stepContentProps: {
+    ...StepContentMock.file,
+    stepContentFile: {
+      ...StepContentFileMock.fileNotFound
+    }
+  },
+}
+
 
 export const Mock = {
   queue,
+  fileSelectAFile,
+  fileNextStep,
+  fileNotFound
 }
